20fcc78e58371aecff6ce372b841b30b092113e1
Vishal kumar <vishalk80269@gmail.com>
2023-10-26 00:41:07 +0530

    fix some bug1

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,28 +1,29 @@
 import React from "react";
-import { BsInstagram } from 'react-icons/bs';
-import { BsFacebook } from 'react-icons/bs';
-
+import { BsInstagram } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
 
 const Footer = () => {
-  return <>
-
-<div className="footeer">
-<div className="row text-center text-white">
-<div className="foot">
-  <div className="companyName">
-    <p><b>ar@biancagarutti.com</b></p>
-  </div>
-  <div className="socialLogo">
-    <BsInstagram color="white" fontSize={40}/>
-  </div>
-  <div className="socialLogo">
-     <BsFacebook color="white" fontSize={40}/>
-  </div>
-</div>
-</div>
-</div>
-  
-  </>;
+  return (
+    <>
+      <div className="footeer">
+        <div className="row text-center text-white">
+          <div className="foot">
+            <div className="companyName">
+              <p>
+                <b>ar@biancagaruttid.com</b>
+              </p>
+            </div>
+            <div className="socialLogo">
+              <BsInstagram color="white" fontSize={40} />
+            </div>
+            <div className="socialLogo">
+              <BsFacebook color="white" fontSize={40} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
