414fd6aadf1e67413cca25776514f086a8e239b9
Vishal kumar <vishalk80269@gmail.com>
2023-10-26 00:19:44 +0530

    fix some code

--- src/component/App.js
+++ src/component/App.js
@@ -1,5 +1,5 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "../pages/Home";
 import About from "../pages/About";
