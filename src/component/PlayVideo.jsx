e74afd87cb15a103b76ea11459bbf182160f5f73
Vishal kumar <vishalk80269@gmail.com>
2023-12-06 20:20:10 +0530

    video tag add in home page

--- src/component/PlayVideo.jsx
+++ src/component/PlayVideo.jsx
@@ -1,31 +1,55 @@
 import React from "react";
-import ReactPlayer from "react-player/lazy";
+// import ReactPlayer from "react-player/lazy";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+import { EffectCube, Pagination } from "swiper/modules";
 
 const PlayVideo = () => {
-  const arr = [1, 2, 3];
+  // const arr = [1, 2, 3];
   return (
     <>
-      <div className="playVideo pt-5">
-        <div className="container py-5">
-          <div className="row d-flex justify-content-between flex-wrap-reverse atMobileDevice">
-            {arr.map((x, i) => (
-              <div className="col-md-3 playVideo_auto" key={i}>
-                <ReactPlayer
-                  url="ttps://www.youtube.com/watch?v=LXb3EKWsInQ"
-                  height={350}
-                  width={250}
-                  style={{
-                    border: "10px solid white",
-                    borderRadius: "30px",
-                    padding: 0,
-                    margin: "1vh",
-                  }}
-                  playing={true}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
+      <div className="homeSlider">
+        <Swiper
+          effect={"cube"}
+          grabCursor={true}
+          cubeEffect={{
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94,
+          }}
+          pagination={true}
+          modules={[EffectCube, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <video autoPlay muted height="300px" width="100%">
+              <source
+                src="https://media.geeksforgeeks.org/wp-content/uploads/20190616234019/Canvas.move_.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <video autoPlay muted height="300px" width="100%">
+              <source
+                src="https://media.geeksforgeeks.org/wp-content/uploads/20190616234019/Canvas.move_.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </SwiperSlide>
+          <SwiperSlide>
+            <video autoPlay muted height="300px" width="100%">
+              <source
+                src="https://media.geeksforgeeks.org/wp-content/uploads/20190616234019/Canvas.move_.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </>
   );
